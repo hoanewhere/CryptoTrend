cbf8cfb830f557f4f5cf717cc7b06e6e54c43891
hoandwhere <e.h0andwhere@gmail.com>
2019-09-05 06:44:37 +0000

    mysqlの設定を追加

--- config/database.php
+++ config/database.php
@@ -42,12 +42,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => env('DB_DATABASE', 'crypto_trend'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'Ragiann10;'),
+            'unix_socket' => env('DB_SOCKET', '/var/lib/mysql/mysql.sock'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
