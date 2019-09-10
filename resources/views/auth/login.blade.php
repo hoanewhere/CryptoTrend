a4d6ff66a0c778e4ea4a687964d1f2ce4eab7fd9
hoandwhere <e.h0andwhere@gmail.com>
2019-09-10 23:51:28 +0000

    ログイン画面を自作のものに置き換え

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.crypto')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="c-form">
+    <h1 class="c-form_title u-fz-lg">ログイン</h1>
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <div class="c-form_contents">
+            <div class="u-mb-lg">
+                <label for="email" class="u-mb-sm">Email</label>
+                <input type="email" name="email" id="email" value="{{ old('email') }}" class="c-input_text @error('email') c-invalid_input-text @enderror" required autocomplete="email" autofocus>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                @error('email')
+                    <span class="c-invalid_text" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="u-mb-lg">
+                <label for="password" class="u-mb-sm">パスワード</label>
+                <input type="password" name="password" id="password" class="c-input_text @error('password') is-invalid @enderror" required autocomplete="current-password">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @error('password')
+                    <span class="c-invalid_text" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="c-form_content-row u-mb-lg">
+                <input type="checkbox" name="remember" id="remember" class="u-mr-md" {{ old('remember') ? 'checkd' : '' }}>
+                <label for="remember">ログイン状態を保持する</label>
+            </div>
+            <div class="c-form_content-row u-mb-lg">
+                <button type="submit" class="c-button c-button-peace u-mb-md">ログイン</button>
+                <a href="{{ route('password.request') }}" class="c-button c-button_link u-mb-md">パスワードを忘れた方</a>
             </div>
         </div>
-    </div>
+    </form>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/crypto/login', 'CryptoController@test_login');
+// Route::get('/crypto/login', 'CryptoController@test_login');
 Route::get('/crypto/register', 'CryptoController@test_register');
 Route::get('/index', 'CryptoController@index');
 Route::get('/accountList', 'CryptoController@accountList');
