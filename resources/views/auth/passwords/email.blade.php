45186eba758539ee8b6f716386c80492cef35c55
hoandwhere <e.h0andwhere@gmail.com>
2019-09-11 00:23:41 +0000

    パスワードリマインダー画面を自作のものに置き換え

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,26 @@
-@extends('layouts.app')
+@extends('layouts.crypto')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="c-form">
+    <h1 class="c-form_title u-fz-lg">パスワードリセット</h1>
+    <form action="{{ route('password.email') }}" method="POST">
+        @csrf
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="c-form_contents">
+            <div class="u-mb-lg">
+                <label for="email" class="u-mb-sm">Email</label>
+                <input type="email" name="email" id="email" class="c-input_text @error('email') c-invalid_input-text @enderror" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @error('email')
+                    <span class="c-invalid_text" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="u-mb-lg">
+                    <button type="submit" class="c-button c-button-peace u-mb-md">パスワードリセットメール送信</button>
             </div>
         </div>
-    </div>
+    </form>
 </div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,39 @@
-@extends('layouts.app')
+@extends('layouts.crypto')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="c-form">
+    <h1 class="c-form_title u-fz-lg">パスワードリセット</h1>
+    <form action="{{ route('password.update') }}" method="POST">
+        @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+        <div class="c-form_contents">
+            <div class="u-mb-lg">
+                <label for="email" class="u-mb-sm">Email</label>
+                <input type="email" name="email" id="email" value="{{ $email ?? old('email') }}" class="c-input_text @error('email') c-invalid_input-text @enderror" required autocomplete="email" autofocus>
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+                @error('email')
+                    <span class="c-invalid_text" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="u-mb-lg">
+                <label for="password" class="u-mb-sm">パスワード</label>
+                <input type="password" name="password" id="password" class="c-input_text @error('password') is-invalid @enderror" required autocomplete="new-password">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @error('password')
+                    <span class="c-invalid_text" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror            </div>
+            <div class="u-mb-lg">
+                <label for="rePassword" class="u-mb-sm">Re:パスワード</label>
+                <input type="password" name="rePassword" id="rePassword" class="c-input_text" required autocomplete="new-password">
+            </div>
+            <div class="u-mb-lg">
+                    <button type="submit" class="c-button c-button-peace u-mb-md">パスワードリセット</button>
             </div>
         </div>
-    </div>
+    </form>
 </div>
 @endsection
