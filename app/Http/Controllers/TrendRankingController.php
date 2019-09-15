27226f4825d17b00d3b170418996b56456209f4c
hoandwhere <e.h0andwhere@gmail.com>
2019-09-15 02:02:21 +0000

    ツイート数集計機能を一部実装 ・新規集計時の処理をDB接続なし版で実装
    →残りの実装
    ・DB接続絡み
    ・２回目以降のツイート集計処理

--- app/Http/Controllers/TrendRankingController.php
+++ app/Http/Controllers/TrendRankingController.php
@@ -9,39 +9,137 @@ use App\Http\Controllers\CoinCheckController;
 
 class TrendRankingController extends Controller
 {
+
+
+
+
     public function index() {
-        $hogehoge =  $this->searchTrends(1);
-        Log::debug('$trends: '.print_r($hogehoge, true));
+        // $hogehoge =  $this->searchTrends(1);
+        // Log::debug('$trends: '.print_r($hogehoge, true));
+
+        $this->aggregateTweetTrend();
         return view('crypto.trendRanking');
     }
 
 
     /**
      * 仮想通貨のトレンド（ツイート数と取引価格）を取得する。
-     * @param int $term_id
+     * 
      * @return array $trends
      */
-    private function searchTrends(int $term_id) {
-        $trends = array();
+    // private function searchTrends() {
+    //     Log::debug('searchTrends(関数呼び出し)');
+    //     $trends = array();
 
-        // cryptoテーブルから対象のcryptoを取得 
-        $crypto_list = ['BTC', 'AAA'];// TBD: $crypto_list =; TBD:テーブルから読み出し
+    //     // cryptoテーブルから対象のcryptoを取得 
+    //     $crypto_list = ['BTC', 'AAA'];// TBD: $crypto_list =; TBD:テーブルから読み出し
+
+    //     // 仮想通貨の数だけループする
+    //     foreach($crypto_list as $crypto) {
+    //         // 条件からツイート情報取得
+    //         $searched_tweet = TwitterController::countTweet($crypto); // TBD: メソッド未実装
+    //         $tweet_cnt = $searched_tweet;// TBD: $searched_tweetからツイート数抜き出す
+
+    //         // 仮想通貨の取引価格を取得
+    //         $search_price = CoinCheckController::searchTransactionPrice($crypto); // TBD: メソッド未実装
+    //         $crypto_max = $search_price; // TBD: $search_priceから最大取引価格を取り出す
+    //         $crypto_min = $search_price; // TBD: $search_priceから最小取引価格を取り出す
+
+    //         $trends[$crypto] = ['tweet_cnt'=>$tweet_cnt, 'transaction_price_max'=>$crypto_max, 'transaction_price_min'=>$crypto_min];
+    //         Log::debug('$trends: '.print_r($trends, true));
+    //     }
+    //     Log::debug('$trends: '.print_r($trends, true));
+    //     return $trends;
+    // }
+
+    public function aggregateTweetTrend() {
+        Log::debug('aggregateTweetTrend(関数呼び出し)');
+        // 現在の日付でtimeテーブル呼び出し
+        $time_flg = false; // TBD: タイムテーブル呼び出してあるかないかを判定
+        $complete_flg = false; // TBD: timeテーブルの完了フラグをセット
+
+        if($time_flg) { //現在日付のレコードがある場合
+            if ($complete_flg) { // 現在日付の情報取得が完了している場合
+                return;
+            } else {
+                $start_day = '2019-09-11 00:00:00'; // TBD: timestampからstringに変換。右記参照 $start_day = date('Y-m-d_H:i:s', strtotime(timeテーブルのtimestamp));
+                $crypto_data = $this->resumeSearch($start_day); //TBD: 再開処理開始
+            }
+        } else { //現在日付のレコードがない場合
+            // TBD: timeテーブルにレコード追加
+            
+            $start_day = date("Y-m-d H:i:s");
+            $crypto_data = $this->startSearch($start_day); // TBD: 新規処理開始
+        }
+
+        // 集計結果をもとにDB更新
+        //TBD:
+        Log::debug('集計結果: '.print_r($crypto_data, true));
+
+        // 集計が完了したか確認する
+        foreach($crypto_data as $item) {
+            if($item['complete_flg'] === false) {
+                return; // 一つでも未完了のものがあれば処理終了
+            }
+        }
+        // 全て完了の場合、timeテーブルのcomplete_flgを完了にして処理終了
+        // TBD:
+
+    }
+
+    private function startSearch(string $start_day) {
+        Log::debug('startSearch(関数呼び出し)');
+
+        $crypto_list = ['月代', '匕首']; // TBD: crypto_list取得 ←　別関数
+        $crypto_data = $this->createCryptoData($crypto_list); // crypto_listからtmp生成
+        $crypto_data = $this->searchForDetails($start_day, $crypto_data, $crypto_list);
+        return $crypto_data;
+    }
+
+    private function resumeSearch(string $start_day) {
+        Log::debug('resumeSearch(関数呼び出し)');
+
+        $crypto_list = array(); // TBD: crypto_list取得 ←　別関数(getCryotoList)
+        $crypto_data = array(); // DBからデータ取得してtmp生成
+        $crypto_data = $this->searchForDetails($start_day, $crypto_data, $crypto_list);
+        return $crypto_data;
+    }
+
+    private function searchForDetails(string $start_day, array $crypto_data, array $crypto_list){
+        Log::debug('searchForDetails(関数呼び出し)');
+        Log::debug('引数:start_day:' . $start_day);
+        Log::debug('引数:crypto_data:' . print_r($crypto_data, true));
+        Log::debug('引数:crypto_list:' . print_r($crypto_list, true));
 
         // 仮想通貨の数だけループする
-        foreach($crypto_list as $crypto) {
-            // 条件からツイート情報取得
-            $searched_tweet = TwitterController::searchTweet($crypto, $term_id); // TBD: メソッド未実装
-            $tweet_cnt = $searched_tweet;// TBD: $searched_tweetからツイート数抜き出す
+        foreach($crypto_data as $key => &$data) {
+            Log::debug('ループのdata:' . print_r($data, true));
+            // 条件からツイート数を取得
+            if ($data['complete_flg'] == false) {
+                $data = TwitterController::countTweet($key, $start_day, $data); // TBD: メソッド未実装
+            }
 
-            // 仮想通貨の取引価格を取得
-            $search_price = CoinCheckController::searchTransactionPrice($crypto); // TBD: メソッド未実装
-            $crypto_max = $search_price; // TBD: $search_priceから最大取引価格を取り出す
-            $crypto_min = $search_price; // TBD: $search_priceから最小取引価格を取り出す
-
-            $trends[$crypto] = ['tweet_cnt'=>$tweet_cnt, 'transaction_price_max'=>$crypto_max, 'transaction_price_min'=>$crypto_min];
-            Log::debug('$trends: '.print_r($trends, true));
+            // twitterの制限状態を確認(フラグが立っている場合、下ろしてループ抜ける)
+            if (TwitterController::$searchd_limit_flg == true) {
+                TwitterController::$searchd_limit_flg = false;
+                break;
+            }
         }
-        Log::debug('$trends: '.print_r($trends, true));
-        return $trends;
+        return $crypto_data;
+    }
+
+    private function createCryptoData(array $list) {
+        $cyrpto_data = array();
+
+        foreach($list as $item) {
+            $cyrpto_data[$item] = [
+                'hour_cnt' => 0,
+                'day_cnt' => 0,
+                'week_cnt' => 0,
+                'complete_flg' => false,
+                'next_params' => ''
+            ];
+        }
+        return $cyrpto_data;
     }
 }
--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Log;
 
 class TwitterController extends Controller
 {
-    const MAX_SEARCH = 450;
+    const MAX_SEARCH = 14;
 
     private static $searchd_cnt = 0;
     public static $searchd_limit_flg = false;
@@ -19,31 +19,39 @@ class TwitterController extends Controller
      * @param string $word
      * @return array $tweet_cnt
      */
-    public static function countTweet(string $word, string $since, array $data) {
-        // 返り値の初期化
-        $tweet_cnt = ['hour_cnt' => 0, 'day_cnt' => 0, 'week_cnt' => 0, 'error_flg' => False];
+    public static function countTweet(string $word, string $ref_date, array $data) {
+        // twitterAPIのパラメータ初期化
+        $params = array();
 
-        // unixtime取得
-        $ut_one_hour_ago = strtotime("-1 hour");
-        $ut_a_day_ago = strtotime("-1 day");
+        // 計測用のunixtime, date取得
+        $ut_one_hour_ago = strtotime($ref_date . " -1 hour");
+        $ut_a_day_ago = strtotime($ref_date . " -1 day");
+        $string_since_day = date("Y-m-d_H:i:s", strtotime($ref_date . " -7 day"));
 
         // クライアントIDでツイッター認証
         $config = config('twitter');
         $connection = new TwitterOAuth($config['api_key'], $config['secret_key'], $config['access_token'], $config['access_token_secret']);
         
-        // 初回のパラメータを設定
-        $params = ['q' => '老獪 since:' . '2019-09-03', 'count' => 100];
-
-        // 全件取得できるまでループ
-        while(1) {
+        // パラメータを設定
+        if ($data['next_params']){
+            parse_str($data['next_params'], $params);
+        } else {
+            $params = ['q' => $word . ' since:' . $string_since_day, 'count' => 100]; // 例：「BTC since:2019-09-09_00:00:00」で100件検索する
+        }
+        
+        // 全件取得 or アクセス制限の上限までループ
+        for($i=0; $i<self::MAX_SEARCH; $i++) {
+            Log::debug('twitter接続開始:');
+            Log::debug('twitter接続時のパラメータ:'. print_r($params, true));
             $result_std = $connection->get('search/tweets', $params);
             $result = json_decode(json_encode($result_std), true);
             Log::debug('ツイートresult:'. print_r($result, true));
 
-            // レスポンスがエラーで返ってきた場合、error_flgを立てる
+            // レスポンスがエラーで返ってきた場合、limit_flgを立ててループを抜ける
             if (isset($result['errors'])) {
                 Log::debug('resultエラー:'. print_r($result['errors'], true));
-                $tweet_cnt['error_flg'] = true;
+                self::$searchd_limit_flg = true;
+                self::$searchd_cnt = 0;
                 break;
             }
 
@@ -52,30 +60,43 @@ class TwitterController extends Controller
                 $ut_result = strtotime($arr['created_at']); // 取得データの日時をunixtimeに変換
 
                 if ($ut_result >= $ut_one_hour_ago) {
-                    $tweet_cnt['hour_cnt']++;
-                    $tweet_cnt['day_cnt']++;
-                    $tweet_cnt['week_cnt']++;
+                    $data['hour_cnt']++;
+                    $data['day_cnt']++;
+                    $data['week_cnt']++;
                 } else if ($ut_result >= $ut_a_day_ago) {
-                    $tweet_cnt['day_cnt']++;
-                    $tweet_cnt['week_cnt']++;
+                    $data['day_cnt']++;
+                    $data['week_cnt']++;
                 } else {
-                    $tweet_cnt['week_cnt']++;
+                    $data['week_cnt']++;
                 }
             }
 
             // 全件取得できたか判定する
             if (!isset($result['search_metadata']['next_results'])) {
+                $data['next_params'] = "";
+                $data['complete_flg'] = true;
                 break;
             } else {
                 // 先頭の「?」を除去
                 $next_results = preg_replace('/^\?/', '', $result['search_metadata']['next_results']);
+                $data['next_params'] = $next_results;
             }
 
             // next_resultsから次の検索パラメータを設定
-            parse_str($next_results, $params);
+            parse_str($data['next_params'], $params);
+
+            // search回数をカウント
+            self::$searchd_cnt++;
+            if (self::$searchd_cnt >= self::MAX_SEARCH) {
+                self::$searchd_limit_flg = true;
+                self::$searchd_cnt = 0;
+                break;
+            }
         }
 
-        Log::debug('ツイート件数(結果):'. print_r($tweet_cnt, true));
-        return $tweet_cnt;
+        Log::debug('twitterController集計結果:'. print_r($data, true));
+        Log::debug('制限フラグ：'.self::$searchd_limit_flg);
+        Log::debug('制限フラグ：'.self::$searchd_cnt);
+        return $data;
     }
 }
