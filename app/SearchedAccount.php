fc0f163f75273431a4dbd38c888c68ad544582b5
hoandwhere <e.h0andwhere@gmail.com>
2019-09-19 01:56:44 +0000

    DBのカラム追加に合わせてモデルのfillableも追加、修正

--- app/SearchedAccount.php
+++ app/SearchedAccount.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class SearchedAccount extends Model
 {
     protected $fillable = [
-        'twitter_account', 'twitter_user_id', 'profile_img_url', 'follow_cnt', 'follower_cnt', 'discription', 'recent_tweet_json', 'follow_flg', 'login_user_id'
+        'account_data', 'update_time_id', 'login_user_id'
     ];
 }
