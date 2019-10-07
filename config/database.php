cfa0c17769d27a61aabe61cc053840ee4e007a55
hoandwhere <e.h0andwhere@gmail.com>
2019-10-07 22:33:49 +0000

    envファイル修正に合わせてdatabaseのconfig周りを修正

--- config/database.php
+++ config/database.php
@@ -42,12 +42,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'crypto_trend'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'Ragiann10;'),
-            'unix_socket' => env('DB_SOCKET', '/var/lib/mysql/mysql.sock'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
